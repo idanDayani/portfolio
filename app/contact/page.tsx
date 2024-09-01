e1eed094ed96fe1e77d2a1bd4a82e7a07236eafa
IdanDayani8 <idand@yuvital.com>
2024-09-01 22:51:06 +0300

    refactor: Fix typo in Contact page title

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className="flex flex-col my-10 mx-6 lg:m-20 p-2 lg:p-8">
       <div className="text-5xl font-bold tracking-tighter lg:text-6xl/none mb-8 text-yellow-1000">
-        Let's Talk
+        Let&apos;s Talk
       </div>
       <ContactForm />
     </div>
