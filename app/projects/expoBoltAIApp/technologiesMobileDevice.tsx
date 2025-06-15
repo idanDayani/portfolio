148aa9e1bf9d7e9fd70695c7b7f3c253034c5a7b
IdanDayani8 <idand@yuvital.com>
2025-06-15 23:33:01 +0300

    feat: Refactor Projects component to use Tabs for navigation and improve layout

--- app/projects/expoBoltAIApp/technologiesMobileDevice.tsx
+++ app/projects/expoBoltAIApp/technologiesMobileDevice.tsx
@@ -7,11 +7,11 @@ export function TechnologiesMobileDevice() {
         <div className="flex flex-col mt-12 md:mt-10">
             <h2 className="text-3xl text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 md:text-4xl sm:text-left">Technologies used</h2>
             <div className="flex flex-col items-center">
-                <div className="flex items-center gap-16">
+                <div className="flex items-center gap-12">
                     <ExpoLogo />
                     <ReactNativeLogo />
                 </div>
-                <div className="flex items-center">
+                <div className="flex items-center pt-4">
                     <BoltNewLogo />
                 </div>
             </div>
--- app/projects/expoBoltAIApp/technologiesUsed.tsx
+++ app/projects/expoBoltAIApp/technologiesUsed.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useWindowSize } from "usehooks-ts";
 import { screenSizes } from "@/app/common/screenSizes";
-import { ReactLogo } from "@/public/assets/logos/react";
 import { ReactNativeLogo } from "@/public/assets/logos/reactNative";
 import { ExpoLogo } from "@/public/assets/logos/expo";
 import { BoltNewLogo } from "@/public/assets/logos/boltNew";
@@ -19,7 +18,7 @@ export function TechnologiesUsed() {
         <div className="flex flex-col mt-12 md:mt-10">
             <h2 className="text-3xl text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 md:text-4xl sm:text-left">Technologies used</h2>
             <div className="flex flex-col items-center">
-                <div className="flex items-center gap-4 sm:gap-20 md:gap-40">
+                <div className="flex items-center gap-6 sm:gap-20 md:gap-40">
                     <ExpoLogo />
                     <ReactNativeLogo />
                     <BoltNewLogo />
